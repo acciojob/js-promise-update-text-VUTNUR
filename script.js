c93a309f2d214e798c10dbe64a4d047c2104f88d
Vutnur Nethaji Goud <130832771+VUTNUR@users.noreply.github.com>
2023-05-29 17:40:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let prom1= new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve("Hello world!")
+	},1000)
+	
+})
+prom1.then((data)=>{
+	let div= document.getElementById("output")
+	div.innerText=data
+})
